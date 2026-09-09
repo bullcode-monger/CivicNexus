57d02f28fce7b69f21e0b7d88f090164770b3204
MourningRay <boyd8230@gmail.com>
2026-09-09 09:38:01 +0200

    functional register/signup php

--- pages/general/lkdbconnect.php
+++ pages/general/lkdbconnect.php
@@ -2,10 +2,10 @@
 
 $hostname = "is3-dev.ict.ru.ac.za";
 $user ="G23K6015";
-$password = "KinLuc24!"
+$password = "KinLuc24!";
 $dbname = "civicnexus";
 
-$conn = new mysql($hostname,$user,$password,$dbname) or
+$conn = new mysqli($hostname,$user,$password,$dbname) or
 die("Database connection failed". $conn->connect_error);
 Echo "Database Connection Sucessfully Established"
 ?>
